b8e5bee3a52832b2566e162bc284311e446d8823
Robin Gather <rgather11@gmail.com>
2023-05-13 19:42:49 +0200

    securing account api

---
+++ secured_account_api/resources/ticket.py
@@ -0,0 +1,35 @@
+import datetime
+from flask import jsonify
+from daos.ticket_dao import TicketDAO
+from db import Session
+import uuid
+
+
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+class Ticket:
+    @staticmethod
+    def create(body):
+
+        # new_time = (datetime.now() + relativedelta(days=5)).strftime('%d/%m/%Y %H:%M:%S')
+        session = Session()
+        ticket = TicketDAO(datetime.now(), datetime.now(), (datetime.now() + relativedelta(days=7)),
+        "valid", body['price'], str(uuid.uuid4()), body['account_email'])
+        session.add(ticket)
+        session.commit()
+        session.refresh(ticket)
+        session.close()
+        return jsonify({'ticket_id': ticket.id}), 200
+
+    def update(body):
+      pass
+
+    # @staticmethod
+    # def update(d_id, status):
+    #     session = Session()
+    #     delivery = session.query(DeliveryDAO).filter(DeliveryDAO.id == d_id)[0]
+    #     delivery.status.status = status
+    #     delivery.status.last_update = datetime.datetime.now()
+    #     session.commit()
+    #     return jsonify({'message': 'The delivery status was updated'}), 200
