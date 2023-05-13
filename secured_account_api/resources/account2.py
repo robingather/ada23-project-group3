f0a06307651a6beab3912a431fd207fed35fa838
Robin Gather <rgather11@gmail.com>
2023-05-13 20:05:17 +0200

    securing api

--- secured_account_api/resources/account2.py
+++ secured_account_api/resources/account2.py
@@ -13,6 +13,7 @@ class Account:
     def create(post_data):
         session = Session()
         # check if user already exists
+        print("hello")
         user = session.query(AccountDAO).filter(AccountDAO.id == post_data.get('email_address')).first()
         if not user:
             try:
